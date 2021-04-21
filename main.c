bd6517e079fdb4ecd16ed5c9fd0f4119a35ae772
Повелитель МаКаРоН <82654703+Topalo-Denus@users.noreply.github.com>
2021-04-21 12:18:25 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <math.h>
+int main(void){
+int n;
+printf("Write number yors flat- ");
+scanf("%d", &n);
+switch(n)
+{
+case 1: printf("\tyour floor in the flat-1"); break;
+case 2: printf("\tyour floor in the flat-1"); break;
+case 3: printf("\tyour floor in the flat-1"); break;
+case 4: printf("\tyour floor in the flat-1"); break;
+case 5: printf("\tyour floor in the flat-1"); break;
+case 6: printf("\tyour floor in the flat-2"); break;
+case 7: printf("\tyour floor in the flat-2"); break;
+case 8: printf("\tyour floor in the flat-2"); break;
+case 9: printf("\tyour floor in the flat-2"); break;
+case 10: printf("\tyour floor in the flat-2"); break;
+case 11: printf("\tyour floor in the flat-3"); break;
+case 12: printf("\tyour floor in the flat-3"); break;
+case 13: printf("\tyour floor in the flat-3"); break;
+case 14: printf("\tyour floor in the flat-3"); break;
+case 15: printf("\tyour floor in the flat-3"); break;
+case 16: printf("\tyour floor in the flat-4"); break;
+case 17: printf("\tyour floor in the flat-4"); break;
+case 18: printf("\tyour floor in the flat-4"); break;
+case 19: printf("\tyour floor in the flat-4"); break;
+case 20: printf("\tyour floor in the flat-4"); break;
+case 21: printf("\tyour floor in the flat-5"); break;
+case 22: printf("\tyour floor in the flat-5"); break;
+case 23: printf("\tyour floor in the flat-5"); break;
+case 24: printf("\tyour floor in the flat-5"); break;
+case 25: printf("\tyour floor in the flat-5"); break;
+case 26: printf("\tyour floor in the flat-6"); break;
+case 27: printf("\tyour floor in the flat-6"); break;
+case 28: printf("\tyour floor in the flat-6"); break;
+case 29: printf("\tyour floor in the flat-6"); break;
+case 30: printf("\tyour floor in the flat-6"); break;
+case 31: printf("\tyour floor in the flat-7"); break;
+case 32: printf("\tyour floor in the flat-7"); break;
+case 33: printf("\tyour floor in the flat-7"); break;
+case 34: printf("\tyour floor in the flat-7"); break;
+case 35: printf("\tyour floor in the flat-7"); break;
+case 36: printf("\tyour floor in the flat-8"); break;
+case 37: printf("\tyour floor in the flat-8"); break;
+case 38: printf("\tyour floor in the flat-8"); break;
+case 39: printf("\tyour floor in the flat-8"); break;
+case 40: printf("\tyour floor in the flat-8"); break;
+case 41: printf("\tyour floor in the flat-9"); break;
+case 42: printf("\tyour floor in the flat-9"); break;
+case 43: printf("\tyour floor in the flat-9"); break;
+case 44: printf("\tyour floor in the flat-9"); break;
+case 45: printf("\tyour floor in the flat-9"); break;
+}
+if (n<=0){
+printf("\tyou live either on the roof or in the basement");
+}
+if (n>45){
+printf("\tyou live either on the roof or in the basement");
+}
+;
+return 0;
+}
